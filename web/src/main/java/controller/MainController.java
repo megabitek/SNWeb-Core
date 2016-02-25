5d92c7e7ac5366fc0c2b6f9fe22eb074cb18c8d0
Elena_Kholkina <Elena_Kholkina@EPRUSARW1219.saratov.epam.com>
2016-02-25 13:26:12 +0300

    web deploy

--- web/src/main/java/controller/MainController.java
+++ web/src/main/java/controller/MainController.java
@@ -5,13 +5,23 @@
  */
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 /**
  *
  * @author Elena_Kholkina
  */
 @Controller
 public class MainController {
+    
+    @RequestMapping(value = "/userpage", method = RequestMethod.POST)
+    public String viewHome(Model model, HttpServletRequest req) {
+        return "userpage";
+    }
     
 }
